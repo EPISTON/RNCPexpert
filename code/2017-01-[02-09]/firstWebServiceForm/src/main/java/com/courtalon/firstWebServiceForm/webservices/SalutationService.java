0c9652ba347b61a74edc03b68738124c1f72470d
Stagiaire <Stagiaire@192.168.5.51>
2017-01-06 16:39:52 +0100

    ajout d'une methode au service renvoyant des entite de la base

--- code/2017-01-[02-09]/firstWebServiceForm/src/main/java/com/courtalon/firstWebServiceForm/webservices/SalutationService.java
+++ code/2017-01-[02-09]/firstWebServiceForm/src/main/java/com/courtalon/firstWebServiceForm/webservices/SalutationService.java
@@ -7,11 +7,16 @@ import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.jws.soap.SOAPBinding.Use;
 
+import com.courtalon.firstWebServiceForm.metier.Message;
+
 @WebService
 @SOAPBinding(style=Style.DOCUMENT,use=Use.LITERAL)
 public interface SalutationService {
 	
 	@WebMethod
 	String direBonjour(@WebParam(name="nom") String nom);
+	
+	@WebMethod
+	Message findMessageByID(@WebParam(name="id") int id);
 
 }
--- code/2017-01-[02-09]/firstWebServiceForm/src/main/java/com/courtalon/firstWebServiceForm/webservices/SalutationServiceImpl.java
+++ code/2017-01-[02-09]/firstWebServiceForm/src/main/java/com/courtalon/firstWebServiceForm/webservices/SalutationServiceImpl.java
@@ -2,12 +2,26 @@ package com.courtalon.firstWebServiceForm.webservices;
 
 import javax.jws.WebService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.courtalon.firstWebServiceForm.metier.Message;
+import com.courtalon.firstWebServiceForm.repositories.MessageRepository;
+
 @WebService(endpointInterface="com.courtalon.firstWebServiceForm.webservices.SalutationService")
 public class SalutationServiceImpl implements SalutationService {
 
+	@Autowired
+	private MessageRepository messageRepository;
+	
 	@Override
 	public String direBonjour(String nom) {
 		return "bonjour, " + nom;
+	}
+
+	
+	@Override
+	public Message findMessageByID(int id) {
+		return messageRepository.findOne(id);
 	}
 
 }
