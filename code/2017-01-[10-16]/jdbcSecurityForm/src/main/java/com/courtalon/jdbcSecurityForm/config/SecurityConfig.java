0c9bf32c5960af73dcdd4990ae7c63f480f747fd
Stagiaire <Stagiaire@192.168.5.51>
2017-01-13 13:47:37 +0100

    ajout auhtentification via table et jdbc

--- code/2017-01-[10-16]/jdbcSecurityForm/src/main/java/com/courtalon/jdbcSecurityForm/config/SecurityConfig.java
+++ code/2017-01-[10-16]/jdbcSecurityForm/src/main/java/com/courtalon/jdbcSecurityForm/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.courtalon.jdbcSecurityForm.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +14,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity // activer la securité dédiée au web-app
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// authentification avec des compte en mémoire
-		// UNIQUEMENT POUR TEST, PAS EN VRAIS!!!
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("USER", "ADMIN").and()
-			.withUser("vincent").password("toto1234").roles("USER");
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery(
+					"select username,password,enabled from utilisateur where username=?")
+			.authoritiesByUsernameQuery(
+					"select u.username, r.role from utilisateur u"
+					+ " inner join utilisateur_role ur on u.id = ur.utilisateur_id "
+					+ " inner join role r on r.id = ur.role_id"
+					+ " where u.username=?")
+			.dataSource(dataSource);
 		
 	}
 
