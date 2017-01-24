36a0995ac24387e4f6634e305e27fd44dfeff45f
Stagiaire <Stagiaire@192.168.5.51>
2017-01-24 15:33:12 +0100

    ajout test unitaire sur controlleur rest/json plus explications

--- code/2017-01-[02-09]/BoutiqueServiceForm/src/main/java/com/courtalon/BoutiqueServiceForm/web/IndexController.java
+++ code/2017-01-[02-09]/BoutiqueServiceForm/src/main/java/com/courtalon/BoutiqueServiceForm/web/IndexController.java
@@ -1,17 +1,25 @@
 package com.courtalon.BoutiqueServiceForm.web;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.courtalon.BoutiqueServiceForm.metier.Produit;
 import com.courtalon.BoutiqueServiceForm.repositories.ProduitRepository;
 
 @Controller
@@ -44,18 +52,27 @@ public class IndexController {
 		return model;
 
 	}
+
+	@RequestMapping(value = "/bonjour2", method = RequestMethod.POST)
+	public ModelAndView hello2(@RequestParam("name") String name) {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("bonjour");
+		model.addObject("message", "bonjour " + name);
+		return model;
+	}
+
+	
 	//---------------------------------------------
 	@Autowired
 	private ProduitRepository produitRepository;
 
-	@RequestMapping(value = "/produit/liste", method = RequestMethod.GET)
-	public ModelAndView listeProduit() {
-
-		ModelAndView model = new ModelAndView();
-		model.setViewName("produitListe");
-		model.addObject("produits", produitRepository.findAll(new PageRequest(1, 5)));
-
-		return model;
+	@RequestMapping(value = "/produit/liste", method = RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public Page<Produit> listeProduit() {
+		List<Produit> produits = new ArrayList<>();
+		produitRepository.findAll().forEach(produits::add);
+		return new PageImpl<Produit>(produits); 
 	}
 
 }
--- code/2017-01-[02-09]/BoutiqueServiceForm/src/test/java/com/courtalon/BoutiqueServiceForm/test/IndexControllerTest.java
+++ code/2017-01-[02-09]/BoutiqueServiceForm/src/test/java/com/courtalon/BoutiqueServiceForm/test/IndexControllerTest.java
@@ -17,15 +17,21 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.courtalon.BoutiqueServiceForm.metier.Produit;
 import com.courtalon.BoutiqueServiceForm.repositories.ProduitRepository;
 import com.courtalon.BoutiqueServiceForm.web.IndexController;
 
 // les methodes utilitaires pour tester un controller spring mvc
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.hamcrest.Matchers.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -60,6 +66,19 @@ public class IndexControllerTest {
 	public void setupMock() {
 		mockindexController = MockMvcBuilders.standaloneSetup(indexController)
 											 .build();
+		reset(produitRepositoryMock);
+	}
+	
+	// methode utilitaire pour obtenir une liste de produits echantillon
+	private List<Produit> getSampleListProduit1() {
+		List<Produit> produits = new ArrayList<>();
+		produits.add(new Produit(1, "biere oceania", 15.5, 0.75));
+		produits.add(new Produit(2, "miel des carpathes", 25.5, 1.75));
+		produits.add(new Produit(3, "quinoa des andes", 29.99, 1.5));
+		produits.add(new Produit(4, "steak de lama", 35.5, 0.95));
+		produits.add(new Produit(5, "tofu tout fou", 10.99, 1.0));
+		produits.add(new Produit(6, "pain sans gluten", 4.99, 0.75));
+		return produits;
 	}
 	
 	@Test
@@ -75,7 +94,44 @@ public class IndexControllerTest {
 		catch(Exception ex) {
 			fail("declenchement d'une exception au test " + ex.getMessage());
 		}
-			
 	}
+
+	@Test
+	public void testBonjour2() {
+		try {
+			mockindexController
+				.perform(
+						post("/bonjour2")
+							.param("name", "patrick")
+						)
+				.andExpect(status().isOk())
+				.andExpect(model().attribute("message", "bonjour patrick"))
+				.andExpect(forwardedUrl("bonjour"));
+		}
+		catch(Exception ex) {
+			fail("declenchement d'une exception au test " + ex.getMessage());
+		}
+	}
+
+	@Test
+	public void testListeProduit() {
+		when(produitRepositoryMock.findAll())
+			.thenReturn(getSampleListProduit1());
+		
+		//-------------------------------------------
+		try {
+			
+			mockindexController
+				.perform(get("/produit/liste"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.content", hasSize(6)))
+				.andExpect(jsonPath("$.content[0].nom", is("biere oceania")));
+		}
+		catch(Exception ex) {
+			fail("declenchement d'une exception au test " + ex.getMessage());
+		}
+	}
+	
 	
 }
