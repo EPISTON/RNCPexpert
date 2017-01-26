e734a4fa9d121bed797d1ba37ce0a9100b780faa
Stagiaire <Stagiaire@192.168.5.51>
2017-01-26 17:21:46 +0100

    premiere modification angular2

--- code/2017-01-[25-30](RIA)/angular2/quickstart/app/app.component.ts
+++ code/2017-01-[25-30](RIA)/angular2/quickstart/app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  id: number;
+  titre: string;
+  contexte: string;
+  priorite: number;
+
+  constructor (id: number, titre: string, contexte: string, priorite: number) {
+    this.id = id;
+    this.titre = titre;
+    this.contexte = contexte;
+    this.priorite = priorite;
+  }
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{titre}}</h1>
+            <p>
+              {{message}}
+            </p>
+            <h2>{{tache.titre}} - {{tache.priorite}}</h2>
+            `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+   titre : string = "todoApp"
+   message : string = 'bienvenue sur todoApp';
+   tache: Todo = new Todo(1, "apprendre angular2", "formation", 4); 
+}
