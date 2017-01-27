a8fb9f8bcd8b920885041e9917792ec8d699c505
Stagiaire <Stagiaire@192.168.5.51>
2017-01-27 14:24:23 +0100

    edition tache et affichage en fonction de la selection

--- code/2017-01-[25-30](RIA)/angular2/todoAngular2/app/app.component.ts
+++ code/2017-01-[25-30](RIA)/angular2/todoAngular2/app/app.component.ts
@@ -1,6 +1,32 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 import { Tache } from'./tache';
 
+
+/*
+
+de selectedTache.titre ---> input.value
+<input value="{{selectedTache.titre}}" placeholder="titre" />
+
+ de selectedTache.titre ---> input.value
+<input [value]="selectedTache.titre" placeholder="titre" />
+
+on a besoin d'importer le module angularForm
+et la directive Input
+dans ce fichier -> Input depuis @angular/core
+dans le fichier app.module
+ajouter la dépendance au module @angular/form
+pour du bidirectionnel
+[(ngModel)]="selectedTache.titre"
+
+
+d'un evenement html --> code typescript
+(click)="onTacheSelect(tache.id)
+
+cette directive desactive l'execution/rendu
+de la balise si la condition est fausse
+ <div *ngIf="selectedTache">
+
+*/
 
 const taches : Tache[] = [
   new Tache(1, "apprendre angular2", "formation", 4),
@@ -15,12 +41,17 @@ const taches : Tache[] = [
   template: `<h1>todoManager</h1>
             <h2>liste des taches restantes</h2>
             <ul class="taches">
-              <li *ngFor="let tache of taches">
+              <li *ngFor="let tache of taches"
+                (click)="onTacheSelect(tache.id)">
                 <span class="badge bleft">{{tache.id}}</span>
                 {{tache.titre}}
                 <span class="badge bright">{{tache.priorite}}</span> 
               </li>
             </ul>
+            <div *ngIf="selectedTache">
+              <label>titre: </label>
+              <input [(ngModel)]="selectedTache.titre" placeholder="titre" />
+            </div>
             `,
   styles: [
     `
@@ -57,20 +88,29 @@ const taches : Tache[] = [
       }
       .bright {
         float: right;
-        background-color: red;
+        background-color: DarkSalmon;
       }
       .taches li:hover {
         border: solid 2px blue;
       }
-      
-      
-      
     `]
 })
 export class AppComponent  {
    taches: Tache[];
+   selectedTache: Tache;
 
    constructor() {
      this.taches = taches;
+     this.selectedTache = null;
+   }
+
+   onTacheSelect(id: number) {
+     for (var i = 0; i < this.taches.length; i++) {
+       if (this.taches[i].id == id) {
+         this.selectedTache = taches[i];
+         return;
+       }
+     }
+     this.selectedTache = null;
    }
 }
--- code/2017-01-[25-30](RIA)/angular2/todoAngular2/app/app.module.ts
+++ code/2017-01-[25-30](RIA)/angular2/todoAngular2/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
+// pour gerer les formulaire dans notre application
+// on import FormsModule depuis '@angular/forms'
+// et on le déclare dans la propriété imports de @NgModule
 
 //
 // le decorateur NgModule nous permet de déclarer un module angular2
@@ -11,7 +15,7 @@ import { AppComponent }  from './app.component';
 // -> declarations , qu'est ce qu'il exporte
 // -> bootstrap, le composant de démarrage (uniquement sur un module racine)
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]  
 })
